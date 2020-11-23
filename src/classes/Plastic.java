c5b52413287253457089084832cf080346199293
Ruebn <rubenmoreno.saladillo@gmail.com>
2020-11-23 11:18:52 +0100

    Creación de la clase Plastic
    Se ha creado la clase Plastic que controlará los elementos

---
+++ src/classes/Plastic.java
@@ -0,0 +1,11 @@
+package classes;
+
+public class Plastic {
+    private final int id; // id del elemento plástico
+    private final String name; // nombre del elemento plástico
+
+    public Plastic(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
